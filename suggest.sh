a2d6fcd4a1df6ce3bbfca848480fa6f42bb36b47
jwerle <joseph.werle@gmail.com>
2022-03-23 16:45:31 -0400

    refactor(): migrate to 'bpkg.json'

--- suggest.sh
+++ suggest.sh
@@ -10,25 +10,26 @@ usage () {
 
 ## main
 suggest () {
-  declare -a local paths=()
-  declare -a local seen=()
-  declare -a local found=()
-  local query="${1}"
+  local found paths seen
+  declare -a paths=()
+  declare -a seen=()
+  declare -a found=()
+  local query="$1"
 
-  case "${query}" in
+  case "$query" in
     -h|--help)
       usage
       return 0
       ;;
 
     -V|--version)
-      echo "${VERSION}"
+      echo "$VERSION"
       return 0
       ;;
 
     *)
       if [ "-" = "${query:0:1}" ]; then
-        echo >&2 "error: Unknown argument \`${query}'"
+        echo >&2 "error: Unknown argument \`$query'"
         return 1
       fi
       ;;
@@ -37,39 +38,40 @@ suggest () {
   ## search path
   {
     local res=""
-    IFS=':' read -a paths <<< "${PATH}"
+    IFS=':' read -r -a paths <<< "$PATH"
     for (( i = 0; i < ${#paths[@]}; ++i )); do
       local path="${paths[$i]}"
-      local let skip=0
+      local skip=0
 
       ## omit non existent paths
-      if ! test -d "${path}"; then
+      if ! test -d "$path"; then
         continue
       else
         for (( n = 0; n < "${#seen[@]}"; ++n )); do
-          if [ "${path}" = "${seen[$n]}" ]; then
+          if [ "$path" = "${seen[$n]}" ]; then
             skip=1;
             break;
           fi
         done
 
         ## check if skip needed
-        if [ "1" = "${skip}" ]; then
+        if [ "1" = "$skip" ]; then
           continue
         fi
       fi
 
       ## mark seen
-      seen+=( "${path}" )
+      seen+=("$path")
 
       ## find in path
-      if res=$(find "${path}" -name "${query}*" -prune -print 2>/dev/null); then
-        if [ -z "${res}" ]; then
+      if res=$(find "$path" -name "$query*" -prune -print 2>/dev/null); then
+        if [ -z "$res" ]; then
           continue
         fi
-        res="$(echo ${res} | tr '\n' ' ')"
+        res="$(echo "$res" | tr '\n' ' ')"
         ## add to found count
-        found+=( $(echo -n "${res}") )
+        # shellcheck disable=SC2207
+        found+=($(echo -n "$res"))
       fi
     done
   }
@@ -77,23 +79,23 @@ suggest () {
   ## get total
   count="${#found[@]}"
 
-  if (( ${count} == 1 )); then
+  if (( count == 1 )); then
     echo "${found[0]}"
-  elif (( ${count} > 0 )); then
-    printf "suggest: found %d result(s)\n" ${count}
+  elif (( count > 0 )); then
+    printf "suggest: found %d result(s)\n" "$count"
     echo
-    for (( i = 0; i < ${count}; ++i )); do
-      printf "%d %s\n" $(echo -n ${found[$i]} | wc -c | tr -d ' ') "${found[$i]}"
+    for (( i = 0; i < count; ++i )); do
+      printf "%d %s\n" "$(echo -n "${found[$i]}" | wc -c | tr -d ' ')" "${found[$i]}"
     done | sort -n | awk '{ print $2 }' | xargs printf '  %s\n'
   else
-    echo "suggest: Couldn't anything to match \`${query}'"
+    echo "suggest: Couldn't anything to match \`$query'"
     return 1
   fi
   return 0
 }
 
 ## export or run
-if [[ ${BASH_SOURCE[0]} != $0 ]]; then
+if [[ ${BASH_SOURCE[0]} != "$0" ]]; then
   export -f suggest
 else
   suggest "$@"
